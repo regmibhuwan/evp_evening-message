3a5411292d10fc82c5044b3a6dc12baa19dc7ada
Bhuwan Regmi <regmibhuwan555@gmail.com>
2025-11-10 03:32:39 -0400

    Change all test emails back to regmibhuwan555@gmail.com

--- config.ts
+++ config.ts
@@ -16,86 +16,86 @@ export const CATEGORY_MAPPINGS: CategoryMapping[] = [
   {
     label: "Anonymous Company Feedback",
     recipientName: "Management",
-    email: "regmibhuwan777@gmail.com",
+    email: "regmibhuwan555@gmail.com",
     phoneExt: ""
   },
   {
     label: "Payroll / CFO Inquiry",
     recipientName: "Payroll Department",
-    email: "regmibhuwan777@gmail.com",
+    email: "regmibhuwan555@gmail.com",
     phoneExt: ""
   },
   {
     label: "Business Operations",
     recipientName: "Operations Team",
-    email: "regmibhuwan777@gmail.com",
+    email: "regmibhuwan555@gmail.com",
     phoneExt: ""
   },
   {
     label: "People Services (HR)",
     recipientName: "HR Department",
-    email: "regmibhuwan777@gmail.com",
+    email: "regmibhuwan555@gmail.com",
     phoneExt: ""
   },
   {
     label: "General Inquiry",
     recipientName: "Office Staff",
-    email: "regmibhuwan777@gmail.com",
+    email: "regmibhuwan555@gmail.com",
     phoneExt: ""
   },
   // Individual Categories
   {
     label: "Broc",
     recipientName: "Broc",
-    email: "regmibhuwan777@gmail.com",
+    email: "regmibhuwan555@gmail.com",
     phoneExt: ""
   },
   {
     label: "Nicole",
     recipientName: "Nicole",
-    email: "regmibhuwan777@gmail.com",
+    email: "regmibhuwan555@gmail.com",
     phoneExt: ""
   },
   {
     label: "Tim",
     recipientName: "Tim",
-    email: "regmibhuwan777@gmail.com",
+    email: "regmibhuwan555@gmail.com",
     phoneExt: ""
   },
   {
     label: "Denise",
     recipientName: "Denise",
-    email: "regmibhuwan777@gmail.com",
+    email: "regmibhuwan555@gmail.com",
     phoneExt: ""
   },
   {
     label: "Alfa",
     recipientName: "Alfa",
-    email: "regmibhuwan777@gmail.com",
+    email: "regmibhuwan555@gmail.com",
     phoneExt: ""
   },
   {
     label: "Luke",
     recipientName: "Luke",
-    email: "regmibhuwan777@gmail.com",
+    email: "regmibhuwan555@gmail.com",
     phoneExt: ""
   },
   {
     label: "Jeff",
     recipientName: "Jeff",
-    email: "regmibhuwan777@gmail.com",
+    email: "regmibhuwan555@gmail.com",
     phoneExt: ""
   },
   {
     label: "Troy",
     recipientName: "Troy",
-    email: "regmibhuwan777@gmail.com",
+    email: "regmibhuwan555@gmail.com",
     phoneExt: ""
   },
   {
     label: "Dean",
     recipientName: "Dean",
-    email: "regmibhuwan777@gmail.com",
+    email: "regmibhuwan555@gmail.com",
     phoneExt: ""
   }
 ];
